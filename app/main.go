2035a73a98186167908d3f14f905a28212339f84
AhmadDabour <ahmadmdabour@gmail.com>
2026-06-30 14:02:32 -0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,6 +7,9 @@ import (
 	"strings"
 	"bufio"
 	"strconv"
+	"time"
+	"slices"
+	"io"
 )
 
 var _ = net.Listen
@@ -33,20 +36,40 @@ func handleConnection(c net.Conn)  {
 	buff := make([]byte, 1024)
 	for {
 		n, err := c.Read(buff)
-		if err != nil {
-			fmt.Println("Error reading input: ")
+		if err != nil && err != io.EOF {
+			fmt.Println("Error reading input: ", err.Error())
 			break
 		}
 		result := respParser(string(buff[:n]))
 		switch result[0] {
-			case "PING":
+			case "ping":
 				c.Write([]byte("+PONG\r\n"))
-			case "ECHO":
+			case "echo":
 				c.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(result[1]), result[1])))
-			case "SET":
+			case "set":
 				data[result[1]] = result[2]
 				c.Write([]byte("+OK\r\n"))
-			case "GET":
+				if len(result) > 3 { 
+					if result[3] == "ex" {
+						ex, err := strconv.ParseInt(string(result[4]), 10, 64)
+						if err != nil {
+							fmt.Println("Error parsing int")
+						}
+						time.AfterFunc(time.Duration(ex)*time.Second, func() { 
+							delete(data, result[1])
+					})
+					}
+					if result[3] == "px" {
+						ex, err := strconv.ParseInt(result[4], 10, 64)
+						if err != nil {
+							fmt.Println("Error parsing int")
+						}
+						time.AfterFunc(time.Duration(ex)*time.Millisecond, func() {
+							delete(data, result[1])
+						})
+					}
+				}
+			case "get":
 				if res, ok := data[result[1]]; !ok {
 					c.Write([]byte("$-1\r\n"))
 				} else { 
@@ -61,7 +84,6 @@ func respParser(buff string) []string{
 	s, _ := reader.ReadByte()
 	if s != '*' {
 		fmt.Println("Error parsing command")
-		return nil
 	}
 	b, _ := reader.ReadByte() 
 	sliceSize, err := strconv.ParseInt(string(b), 10, 64)
@@ -76,13 +98,11 @@ func respParser(buff string) []string{
 	p, _ := reader.ReadByte()
 	if p != '$' {
 		fmt.Println("Error parsing command")
-		return nil
 	}
 	size, _ := reader.ReadByte()
 	strSize, err := strconv.ParseInt(string(size), 10, 64)
 	if err != nil {
 		fmt.Println("Error parsing int: ", err.Error())
-		return nil
 	}
 	reader.ReadByte()
 	reader.ReadByte()
@@ -90,6 +110,14 @@ func respParser(buff string) []string{
 	reader.Read(resp)
 	input = append(input, string(resp))
 	}
-	strings.ToLower(input[0])
+	input[0] = strings.ToLower(input[0])
+	if slices.Contains(input, "EX") {
+		ind := slices.Index(input, "EX")
+		input[ind] = strings.ToLower(input[ind])
+	}
+	if slices.Contains(input, "PX") {
+		ind := slices.Index(input, "PX")
+		input[ind] = strings.ToLower(input[ind])
+	}
 	return input
 }
